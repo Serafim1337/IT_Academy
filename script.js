07338f241dc7479f026e93904e678effecf86dfb
Serafim <7620226@gmail.com>
2023-03-23 23:23:01 +0300

    feat: implemented treesum

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-let firstName;
-let lastName;
-let secondName;
-let age = 0;
-let gender;
-
-while (!firstName) {
-  firstName = prompt("*Ваше имя:");
-}
-
-while (!secondName) {
-  secondName = prompt("*Ваше отчество:");
-}
-
-while (!lastName) {
-  lastName = prompt("*Ваша фамилия:");
-}
-
-do {
-  age = +prompt("*Ваш возраст:");
-} while (!isFinite(age) || age < 0 || age > 100 || !age);
-
-gender = confirm("*Ваш пол мужской? (ОК - да, Отмена - нет)");
-
-alert(`ваше ФИО: ${firstName} ${secondName} ${lastName}
-ваш возраст в годах: ${age}
-ваш возраст в днях: ${age * 365} 
-через 5 лет вам будет: ${age + 5} 
-ваш пол: ${gender === true ? "мужской" : "женский"} 
-вы на пенсии: ${isRetired(age, gender) ? "да" : "нет"}`);
-
-function isRetired(age, gender) {
-  switch (gender) {
-    case true:
-      if (age > 60) {
-        return true;
-      } else {
-        return false;
-      }
-    case false:
-      if (age > 55) {
-        return true;
-      } else {
-        return false;
-      }
+function treeSum(arr) {
+  let sum = 0;
+  for (let item of arr) {
+    if (!Array.isArray(item)) {
+      console.log("number:" + item);
+      sum += +item;
+    } else {
+      console.log("array:" + item);
+      sum += treeSum(item);
+    }
   }
+  return sum;
 }
+
+console.log("sum=" + treeSum([5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]));
