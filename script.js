f6f871b6b5aa0cb52eb3e22f0fffb9f029441d7d
Serafim <7620226@gmail.com>
2023-03-24 20:15:18 +0300

    refactor: delete logs

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ function treeSum(arr) {
   let sum = 0;
   for (let item of arr) {
     if (!Array.isArray(item)) {
-      console.log("number : " + item);
       sum += +item;
     } else {
-      console.log("array : " + item);
       sum += treeSum(item);
     }
   }
   return sum;
 }
 
-console.log("sum= " + treeSum([5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]));
+console.log("sum = " + treeSum([5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]));
