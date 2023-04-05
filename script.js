535e9220c788f7f8f48787a05b10593c228f8f58
Serafim <7620226@gmail.com>
2023-04-05 20:23:07 +0300

    feat: add foreach, filter, reduce

--- script.js
+++ script.js
@@ -28,6 +28,51 @@ function vowels2(str) {
   return counter;
 }
 
+function vowels3(str) {
+  const set = new Set(["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]);
+  const checkString = str.toLowerCase();
+  let counter = 0;
+
+  checkString.split('').forEach(item => {
+    if(set.has(item)) {
+      counter++
+    }
+  });
+
+  return counter;
+}
+
+function vowels4(str) {
+  const set = new Set(["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]);
+  const checkString = str.toLowerCase();
+
+  const filterResults = checkString.split('').filter(item=>set.has(item));
+
+  const vowelsCount = filterResults.length;
+
+  return vowelsCount;
+}
+
+function vowels5(str) {
+  let set = new Set(["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]);
+  const checkString = str.toLowerCase();
+  
+  const counter = checkString.split('').reduce((acc,curr)=>{
+    if(set.has(curr)) {
+      acc++;
+    }
+    return acc;
+  },0);
+
+  return counter;
+}
+
 console.log("vovels1 result : " + vowels1(userString));
 
 console.log("vovels2 result : " + vowels2(userString));
+
+console.log("vovels3 result : " + vowels3(userString));
+
+console.log("vovels4 result : " + vowels4(userString));
+
+console.log("vovels5 result : " + vowels5(userString));
