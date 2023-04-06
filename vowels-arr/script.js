1b4522aa8aa6f51494f4ff374494e98d5bce74cb
Serafim <7620226@gmail.com>
2023-04-06 16:05:25 +0300

    delete old funcs

--- vowels-arr/script.js
+++ vowels-arr/script.js
@@ -1,33 +1,5 @@
 const userString = prompt("Your string?");
 
-function vowels1(str) {
-  const rusVowels = ["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"];
-  const checkString = str.toLowerCase();
-  let counter = 0;
-
-  for (let i = 0; i < checkString.length; i++) {
-    if (rusVowels.includes(checkString[i])) {
-      counter++;
-    }
-  }
-
-  return counter;
-}
-
-function vowels2(str) {
-  let set = new Set(["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]);
-  const checkString = str.toLowerCase();
-  let counter = 0;
-
-  for (let item of checkString) {
-    if (set.has(item)) {
-      counter++;
-    }
-  }
-
-  return counter;
-}
-
 function vowels3(str) {
   const set = new Set(["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]);
   const checkString = str.toLowerCase();
@@ -66,10 +38,6 @@ function vowels5(str) {
 
   return counter;
 }
-
-console.log("vovels1 result : " + vowels1(userString));
-
-console.log("vovels2 result : " + vowels2(userString));
 
 console.log("vovels3 result : " + vowels3(userString));
 
